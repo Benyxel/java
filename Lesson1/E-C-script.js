4512d08b3a8553e7a7be000bdf983fb6e93712df
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-05-29 23:57:57 +0000

    new

--- Lesson1/E-C-script.js
+++ Lesson1/E-C-script.js
@@ -1,13 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuIcon = document.querySelector('#mobile .bx-menu');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const menuIcon = document.querySelector('#mobile .bx-menu');
+//   const closeIcon = document.querySelector('#navbar #close');
+//   const navbar = document.querySelector('#navbar');
+
+//   menuIcon.addEventListener('click', () => {
+//     navbar.style.right = '0px';
+//   });
+
+//   closeIcon.addEventListener('click', () => {
+//     navbar.style.right = '-300px';
+//   });
+// });
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.querySelector('#mobile .men');
   const closeIcon = document.querySelector('#navbar #close');
   const navbar = document.querySelector('#navbar');
 
-  menuIcon.addEventListener('click', () => {
-    navbar.style.right = '0px';
+  menuIcon.addEventListener('click', function () {
+    navbar.classList.toggle('active');
   });
 
-  closeIcon.addEventListener('click', () => {
-    navbar.style.right = '-300px';
+  closeIcon.addEventListener('click', function () {
+    navbar.classList.remove('active');
   });
 });
+
